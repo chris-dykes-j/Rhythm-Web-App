84e9415d28eeb412f9f2863814808748a62a3df3
Chris Dykes <chris.t.c.dykes@gmail.com>
2022-09-24 12:08:42 -0400

    Initialise project frontend

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+let timeSignature: number, subDivision: number, totalNotes: number;
+
+// Validates input, and fetches api.
+document.getElementById("rhythm-build").addEventListener("submit", e => {
+  e.preventDefault();
+  getInput();
+  validateInput();
+  getRhythm();
+});
+
+// Input method.
+function getInput() {
+  document.getElementsByName("time-sig").forEach(option => {
+    if ((<HTMLInputElement>option).checked)
+      timeSignature = parseInt((<HTMLInputElement>option).value);
+  });
+  document.getElementsByName("sub-div").forEach(option => {
+    if ((<HTMLInputElement>option).checked)
+      subDivision = parseInt((<HTMLInputElement>option).value);
+  });
+  totalNotes = parseInt((<HTMLInputElement>document.getElementById("notes")).value);
+}
+
+// Validation method
+function validateInput(): boolean {
+  if (timeSignature === undefined || subDivision === undefined || isNaN(totalNotes)) // Inputs are not empty.
+    return false;  
+  if (totalNotes >= timeSignature * subDivision || totalNotes === 0) // Is within range. 
+    return false;
+  return true;
+}
+
+// If valid, fetch with api url
+async function getRhythm() {
+  fetch(`http://localhost:8080/${timeSignature}/${subDivision}/${totalNotes}`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}

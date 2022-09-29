656b394c353f191f7a3fc4ddf8daa3cacd379407
Chris Dykes <chris.t.c.dykes@gmail.com>
2022-09-29 17:08:04 -0400

    Fix method instructions. No more errors

--- server/RhythmApi/RhythmCalculator.cs
+++ server/RhythmApi/RhythmCalculator.cs
@@ -5,6 +5,7 @@ public class RhythmCalculator
    private readonly int _timeSignature;
    private readonly int _subDivision;
    private int _totalNotes;
+   
    public RhythmCalculator(int timeSignature, int subDivision, int totalNotes)
    {
       _timeSignature = timeSignature;
@@ -13,36 +14,31 @@ public class RhythmCalculator
    }
 
    // Creates the rhythm, as a list of numbers.
-   public IEnumerable<int> CalculateRhythm()
+   public int[] CalculateRhythm()
    {
-      var bar = new int[_timeSignature];
-      var options = MakeOptions(_timeSignature);
-      var random = new Random();
+      int[] bar = new int[_timeSignature];
+      List<int> options = MakeOptions(_timeSignature);
+      Random random = new();
       
-      for (int k = _totalNotes; k > 0; k--)
+      while (_totalNotes > 0) 
       {
-         int i;
-         try
-         {
-            i = options[random.Next(0, options.Count - 1)];
-         }
-         catch (ArgumentOutOfRangeException e)
-         {
-            i = 0;
-         }
-
-         Console.WriteLine($"Options length: {options.Count-1}, i: {i}");
-         if (bar[options[i]] == _subDivision)
-            options.Remove(i);
+         int index = random.Next(0, options.Count);
+         int choice = options[index];
+         if (bar[choice] == _subDivision)
+            options.RemoveAt(index);
          else
-            bar[options[i]]++;
+         {
+            bar[choice]++;
+            _totalNotes--;
+         }
       }
       return bar;
    }
 
+   // Creates the list of options, the options being which index to add one.
    private static List<int> MakeOptions(int length)
    {
-      var options = new List<int>();
+      List<int> options = new();
       for (int i = 0; i < length; i++)
          options.Add(i);
       return options;
